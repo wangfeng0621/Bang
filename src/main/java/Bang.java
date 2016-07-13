95c7c2ef91bdfe5e8972ce59190894a440c25130
wangfeng <578627759@qq.com>
2016-07-13 22:57:24 +0800

    初始化游戏成员

--- src/main/java/Bang.java
+++ src/main/java/Bang.java
@@ -4,4 +4,21 @@ package main.java;
  * Created by feng on 2016/7/13.
  */
 public class Bang {
+    public GamerProperties Player[];
+    public int PlayerNumbers;
+    public int Killers;
+    public int Polices;
+    public int Civilians;
+
+    public Bang(int Number, String[] str) {
+        PlayerNumbers = Number;
+        Killers = Number / 4;
+        Polices = Number / 4;
+        Civilians = Number - Killers - Polices;
+        Player = new GamerProperties[Number];
+        String role[]  = {"Killer","Killer","Killer","Civilian","Civilian","Civilian","Civilian","Civilian","Civilian","Police","Police","Police"};
+        for(int i = 0; i < Number; i++){
+            Player[i] = new GamerProperties(str[i],i,role[i]) ;
+        }
+    }
 }
--- src/test/BangTest.java
+++ src/test/BangTest.java
@@ -1,10 +1,34 @@
 package test;
 
+import main.java.Bang;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by feng on 2016/7/13.
  */
 public class BangTest {
+
+    @Test
+    public void should_show_Player_name_when_Player_is_12(){
+        String str = "wang zhang yang li zhou zhao yao he gao mo mu lan liu";
+        String st[] = str.split(" ");
+        String role[]  = {"Killer","Killer","Killer","Civilian","Civilian","Civilian","Civilian","Civilian","Civilian","Police","Police","Police"};
+        Bang bang = new Bang(12,st);
+
+        for(int i = 0;i < 12;i++){
+            assertThat(bang.Player[i].State.toString(), is("Alive"));
+            assertThat(bang.Player[i].Name, is(st[i]));
+            assertThat(bang.Player[i].ID, is(i));
+            assertThat(bang.Player[i].role.toString(), is(role[i]));
+        }
+        assertThat(bang.PlayerNumbers, is(12));
+        assertThat(bang.Killers, is(3));
+        assertThat(bang.Polices, is(3));
+        assertThat(bang.Civilians, is(6));
+
+    }
 
 }
